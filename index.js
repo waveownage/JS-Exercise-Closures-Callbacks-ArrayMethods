8e5129ea9358c12932198eda528bc7f08b971ecf
Andre Freeman <asf@mail.usf.edu>
2020-01-19 20:38:12 -0500

    processContains completed

--- index.js
+++ index.js
@@ -133,9 +133,10 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function processContains(item, list, callback) {
+  return (callback(list.includes(item)))
+  }
+
 
 /**
  * ### Challenge `processDuplicateFree`
@@ -178,8 +179,8 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  runners.forEach
 }
 
 /**
